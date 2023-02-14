0b5481cef1e9307b5a799ccb3aa82f6dcd653876
Folka2134 <folka2134@gmail.com>
2023-02-14 14:40:36 +0000

    built hard-coded notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>test</div>
+    <div className="window flex justify-center pt-20 ">
+      <div className="container h-full w-[800px] bg-white p-6">
+        <div className="header flex justify-between pb-2">
+          <h2 className="text-xl font-bold">
+            Notifications{" "}
+            <span className="bg-blue-900 text-base text-white px-2 rounded">
+              3
+            </span>
+          </h2>
+          <h5>Mark all as read</h5>
+        </div>
+        <div className="notifications">
+          <ul>
+            <li className="my-4 flex bg-blue-100 bg-opacity-30 p-2">
+              <img className="px-5" src="#" alt="avatar" />
+              <div>
+                <p>
+                  Mark Webber reacted to your recent post My first tournament
+                  today!
+                </p>
+                <p>1m ago</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
