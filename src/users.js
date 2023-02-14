89905bde70fb86610ead2eba8ece77227c047245
Folka2134 <folka2134@gmail.com>
2023-02-14 14:39:54 +0000

    added users and avatars

---
+++ src/users.js
@@ -0,0 +1,47 @@
+const users = [
+  {
+    name: "Mark Webber",
+    notification: "reacted to your recent post My first tournament today!",
+    posted: "1m",
+    unread: true,
+  },
+  {
+    name: "Angela Gray",
+    notification: "followed you",
+    posted: "5m",
+    unread: true,
+  },
+  {
+    name: "Jacob Thompson",
+    notification: "has joined your group Chess Club",
+    posted: "1 day",
+    unread: true,
+  },
+  {
+    name: "Rizky Hasanuddin",
+    notification: "sent you a private message",
+    posted: "5 days",
+    message:
+      "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+    unread: false,
+  },
+  {
+    name: "Kimberly Smith",
+    notification: "commented on your picture",
+    posted: "1 week",
+    unread: false,
+  },
+  {
+    name: "Nathan Peterson",
+    notification:
+      "reacted to your recent post 5 end-game strategies to increase your win rate",
+    posted: "2 weeks",
+    unread: false,
+  },
+  {
+    name: "Anna Kim",
+    notification: "left the group Chess Club",
+    posted: "2 weeks",
+    unread: false,
+  },
+];
